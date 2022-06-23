818b295ba9fbcbb9e228ef5f64a9666fc5f7d002
mdluther <mdlphx92@gmail.com>
2022-06-22 18:21:46 -0700

    fixed more css

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -54,9 +54,9 @@ const ExpenseForm = (props) => {
             onChange={dateChangeHandler}
           />
         </div>
-      </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
+        <div className="new-expense__actions">
+          <button type="submit">Add Expense</button>
+        </div>
       </div>
     </form>
   );
